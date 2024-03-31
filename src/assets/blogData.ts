327367f0c0efbc12b6d736c97d2ee68992a209a4
Claes Nymand Nilsson <claes89@gmail.com>
2024-04-01 01:06:01 +0200

    Make filterable blogs

--- src/assets/blogData.ts
+++ src/assets/blogData.ts
@@ -1,21 +1,29 @@
+type Category = "Javascript" | "Python" | "Photography"
+
 type Blog = {
   title: string
   description: string
   date: string
   slug: string
+  category: Category
+  tags: string[]
 }
 
 export const blogs: Blog[] = [
   {
-    title: "Test blog",
-    description: "This is a test blog",
-    date: "2021-08-01",
-    slug: "test-blog",
+    title: "Why I chose React and TanStack in 2024",
+    description: "React is dead. Long live React. Many have proclaimed the death of React, due to exciting new frameworks appearing, such as Svelte, Solid and Vue. After trying them all, I still went with React",
+    date: "2024-03-31",
+    slug: "why-i-chose-react-and-tanstack",
+    category: "Javascript",
+    tags: ["JavaScript", "React", "TanStack", "Frameworks"]
   },
   {
     title: "Yo yo yo!",
     description: "This is a test blog",
     date: "2021-08-02",
     slug: "yoyoyo",
+    category: "Python",
+    tags: [],
   },
 ]
--- src/routes/blog.index.lazy.tsx
+++ src/routes/blog.index.lazy.tsx
@@ -1,25 +1,45 @@
 import { Link, createLazyFileRoute } from "@tanstack/react-router";
 import { blogs } from "@/assets/blogData";
+import { useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Button } from "@/components/ui/button";
 
 export const Route = createLazyFileRoute("/blog/")({
   component: Blog,
 });
 
+// Implement sorting / filtering / pagination
+
 function Blog() {
+  const [category, setCategory] = useState("");
+
   function formatDate(input: string) {
     const date = new Date(input);
     return date.toLocaleDateString("en-US", { day: "numeric", month: "short" });
   }
 
+  const filteredBlogs = category
+    ? blogs.filter((blog) => blog.category === category)
+    : blogs;
+
   return (
-    <div className="mt-5">
-      <ul className="max-w-3xl">
-        {blogs.map((blog) => (
+    <div className="mt-5 md:flex">
+      <ul className="w-full">
+        {filteredBlogs.map((blog) => (
           <li key={blog.slug}>
             <Link to="/blog/$id" params={{ id: blog.slug }}>
-              <p className="text-slate-500 text-sm uppercase">
+              <span className="text-slate-500 mr-3 text-xs uppercase">
                 {formatDate(blog.date)}
-              </p>
+              </span>
+              <span className="text-blue-700 font-bold text-xs uppercase">
+                {blog.category}
+              </span>
               <h1 className="font-[Kurale] text-4xl mb-2">{blog.title}</h1>
               <p className="text-slate-700">{blog.description}</p>
               <hr className="my-4" />
@@ -27,6 +47,22 @@ function Blog() {
           </li>
         ))}
       </ul>
+
+      <div className="md:w-80 md:ml-6">
+        <h2 className="font-bold mb-2 text-slate-600">FILTER BLOGS</h2>
+        <div className="flex space-x-2">
+          <Select value={category} onValueChange={setCategory}>
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="Category" className="w-full" />
+            </SelectTrigger>
+            <SelectContent className="w-full">
+              <SelectItem value="Javascript">JavaScript</SelectItem>
+              <SelectItem value="Python">Python</SelectItem>
+            </SelectContent>
+          </Select>
+          {category && <Button onClick={() => setCategory("")}>Reset</Button>}
+        </div>
+      </div>
     </div>
   );
 }
