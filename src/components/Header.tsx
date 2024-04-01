c60a28a83c93d17779fbefe0c0def2d8efca768e
Claes Nymand Nilsson <claes89@gmail.com>
2024-04-01 09:55:39 +0200

    Add formatting to blog page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
-import { Link } from "@tanstack/react-router"
-import { useState } from "react"
-import SvgMenu from "./svg/SvgMenu"
-import SvgClose from "./svg/SvgClose"
-import { Button } from "./ui/button"
+import { Link } from "@tanstack/react-router";
+import { useState } from "react";
+import SvgMenu from "./svg/SvgMenu";
+import SvgClose from "./svg/SvgClose";
+import { Button } from "./ui/button";
 
 const links = [
   {
@@ -21,75 +21,68 @@ const links = [
     name: "About",
     route: "/about",
   },
-]
+];
 
 export default function Header() {
-  const [menuOpen, setMenuOpen] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false);
 
   function closeMenu() {
-    setMenuOpen(false)
-    document.body.style.overflow = "visible"
+    setMenuOpen(false);
+    document.body.style.overflow = "visible";
   }
 
   function toggleMenu() {
-    document.body.style.overflow = menuOpen ? "visible" : "hidden"
-    setMenuOpen(!menuOpen)
+    document.body.style.overflow = menuOpen ? "visible" : "hidden";
+    setMenuOpen(!menuOpen);
   }
 
   return (
     <>
-      <header className='flex justify-between px-4 py-2 place-items-center max-w-7xl mx-auto'>
-        <Link to='/'>
-          <h1
-            className='font-[Oswald] text-2xl'
-            onClick={closeMenu}>
+      <header className="flex justify-between px-4 py-2 place-items-center max-w-7xl mx-auto">
+        <Link to="/">
+          <h1 className="font-[Oswald] text-2xl" onClick={closeMenu}>
             CLAESNN
           </h1>
         </Link>
 
-        <button
-          onClick={toggleMenu}
-          className='sm:hidden'>
+        <button onClick={toggleMenu} className="sm:hidden">
           {!menuOpen ? <SvgMenu /> : <SvgClose />}
         </button>
 
-        <nav className='hidden gap-4 sm:flex'>
+        <nav className="hidden gap-4 sm:flex">
           {links.map((link) => (
             <Link
               to={link.route}
-              className='[&.active]:underline text-sm'
-              key={link.name}>
+              className="[&.active]:border-b-blue-500 [&.active]:border-b-2 text-sm"
+              key={link.name}
+            >
               {link.name}
             </Link>
           ))}
         </nav>
       </header>
       {menuOpen && (
-        <nav className='w-full z-10 fixed top-[50px] flex'>
+        <nav className="w-full z-10 fixed top-[50px] flex">
           <div
             onClick={closeMenu}
-            className=' bg-gray-900 h-screen flex-grow opacity-40'></div>
-          <aside className='w-[20rem] bg-white h-screen p-3 flex flex-col'>
-            <Link to='/about'>
-              <Button
-                variant='link'
-                className='[&.active]:underline text-foreground text-lg '
-                onClick={closeMenu}>
-                About
-              </Button>
-            </Link>
-            <Link to='/photography'>
-              <Button
-                variant='link'
-                className='[&.active]:underline text-foreground text-lg'
-                onClick={closeMenu}>
-                Photography
-              </Button>
-            </Link>
+            className=" bg-gray-900 h-screen flex-grow opacity-40"
+          ></div>
+          <aside className="w-[20rem] bg-white h-screen p-3 flex flex-col">
+            {links.map((link) => (
+              <Link to={link.route} key={link.name}>
+                <Button
+                  variant="link"
+                  className="[&.active]:underline text-foreground text-lg "
+                  onClick={closeMenu}
+                >
+                  {link.name}
+                </Button>
+              </Link>
+            ))}
           </aside>
         </nav>
       )}
       <hr />
     </>
-  )
+  );
 }
